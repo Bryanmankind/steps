5365157cd66626bdfeb574bcd8a975c9a6b93fa4
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-29 00:47:34 +0100

    steps-up

--- steps-react-app/src/App.js
+++ steps-react-app/src/App.js
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <div>
-      <h1>Hello WOLD 🙌</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button>Privious</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 }
