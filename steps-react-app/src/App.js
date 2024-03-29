0262a431757311c223d6711beb0fd64c68fc54a7
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-29 09:36:48 +0100

    steps-up

--- steps-react-app/src/App.js
+++ steps-react-app/src/App.js
@@ -1,16 +1,39 @@
+import {useState} from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+
 export default function App() {
-  return (
+
+  const {step, setStep} = useState(1);
+
+  function handlePrevious  () {
+    alert('Previous')
+  }
+  function handleNext  () {
+    setStep(step + 1);
+  }
+
+   return (
     <div className="steps">
       <div className="numbers">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <p className="message">Hello</p>
+      <p className="message">Step {step} : {messages[step -1 ]}</p>
       <div className="buttons">
-        <button>Privious</button>
-        <button>Next</button>
+        <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handlePrevious}>
+          Privious
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "white" }} onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
