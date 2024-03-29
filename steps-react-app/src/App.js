c8b5d9978a99d88a6c2fd3a1f44cfe9c280a0f59
Makinde Oluwasegun <bryanconquer@gmail.com>
2024-03-29 10:51:44 +0100

    steps-up

--- steps-react-app/src/App.js
+++ steps-react-app/src/App.js
@@ -10,18 +10,22 @@ const messages = [
 export default function App() {
 
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious  () {
-   if (step > 1 ) setStep(step - 1);
+   if (step > 1 ) setStep((s) => s - 1);
   }
 
   function handleNext  () {
-    if (step < 3 ) setStep(step + 1);
+    if (step < 3 ) setStep((s) => s + 1);
 
   }
 
    return (
-    <div className="steps">
+    <> 
+      <button className="close" onClick={() => setIsOpen((Open) => !Open)}>&times;</button>
+    { isOpen &&
+      <div className="steps">
       <div className="numbers">
         <div className={step >= 1 ? "active" : ""}>1</div>
         <div className={step >= 2 ? "active" : ""}>2</div>
@@ -37,6 +41,7 @@ export default function App() {
           Next
         </button>
       </div>
-    </div>
+    </div>}
+    </>
   );
 }
